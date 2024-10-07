d054c80a30ffd2301be4d9b047dfc4a1a2642786
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-10-08 05:30:34 +0600

    Add carousel

--- AndroidAppClass/app/src/main/java/com/jeeclass/androidappclass/Home.java
+++ AndroidAppClass/app/src/main/java/com/jeeclass/androidappclass/Home.java
@@ -8,6 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.jeeclass.androidappclass.apiClient.ApiClient;
+import com.jeeclass.androidappclass.apiClient.SlideApi;
+import com.jeeclass.androidappclass.model.Slide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class Home extends AppCompatActivity {
 
     @Override
@@ -19,6 +33,39 @@ public class Home extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        SlideApi slideApi = ApiClient.getRetrofit().create(SlideApi.class);
+
+        Call<List<Slide>> call = slideApi.getSlides();
+
+        call.enqueue(new Callback<List<Slide>>() {
+            @Override
+            public void onResponse(Call<List<Slide>> call, Response<List<Slide>> response) {
+
+                if (response.isSuccessful() && response.body() != null) {
+                    List<Slide> slides = response.body();
+
+                    ArrayList<SlideModel> imageList = new ArrayList<>();
+
+                    for (Slide slide : slides){
+                        String relativeImagePath = slide.getImage();
+                        String baseUrl = "https://purbachal.emranhss.com/";
+                        String imageUrl = baseUrl + relativeImagePath;
+
+                        imageList.add(new SlideModel(imageUrl, slide.getTitle(), ScaleTypes.CENTER_CROP));
+                        System.out.println(slide.getImage());
+                    }
+
+                    ImageSlider imageSlider = findViewById(R.id.image_slider);
+                    imageSlider.setImageList(imageList);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Slide>> call, Throwable t) {
+
+            }
         });
     }
 }
